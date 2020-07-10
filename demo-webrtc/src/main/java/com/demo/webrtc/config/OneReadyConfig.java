c8edecf54ded29a309afcb22c6934811ffcbd117
chen.dh <chen.dh>
2020-07-10 11:51:25 +0800

    初始化

---
+++ demo-webrtc/src/main/java/com/demo/webrtc/config/OneReadyConfig.java
@@ -0,0 +1,29 @@
+package com.demo.webrtc.config;
+
+/**
+ * 一对一准备配置
+ */
+public class OneReadyConfig {
+	/** 远端的ID */
+	private String remoteId;
+	/** 远端是否已经准备好 */
+	private boolean status;
+
+	public String getRemoteId() {
+		return remoteId;
+	}
+
+	public void setRemoteId(String remoteId) {
+		this.remoteId = remoteId;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	
+}
